723a487dd8cdc6af81c51c22d2b549502efa3157
petr <petro357@rambler.ru>
2024-03-06 11:57:11 +0300

    Test

---
+++ Class 1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        // место для вашего кода
+
+        System.out.println("Значение а: " + a + ". Значение b: " + b);
+    }
+}
